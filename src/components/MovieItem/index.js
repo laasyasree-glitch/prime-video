b50dd2973e137ddd8c86b82d6262dd88b3442089
laasyasree-glitch <laasyakothamasu@gmail.com>
2022-10-23 16:18:35 +0000

    side arrows updated

---
+++ src/components/MovieItem/index.js
@@ -0,0 +1,55 @@
+import Popup from 'reactjs-popup'
+import {IoMdClose} from 'react-icons/io'
+
+import 'reactjs-popup/dist/index.css'
+
+import ReactPlayer from 'react-player'
+
+import './index.css'
+
+const MovieItem = props => {
+  const {details} = props
+  const {thumbnailUrl, videoUrl} = details
+  return (
+    <li className="listItem">
+      <div className="popup-container">
+        <Popup
+          modal
+          trigger={
+            <button className="trigger-button" type="button">
+              <img
+                className="thumbnailUrl"
+                src={thumbnailUrl}
+                alt="thumbnail"
+              />
+            </button>
+          }
+          className="popup-content"
+        >
+          {close => (
+            <div className="card">
+              <div className="player-wrapper">
+                <ReactPlayer
+                  className="react-player"
+                  url={videoUrl}
+                  width="100%"
+                  height="100%"
+                />
+              </div>
+              <div className="closeButtonContainer">
+                <button
+                  type="button"
+                  className="trigger-button"
+                  onClick={() => close()}
+                >
+                  <IoMdClose />
+                </button>
+              </div>
+            </div>
+          )}
+        </Popup>
+      </div>
+    </li>
+  )
+}
+export default MovieItem
---
+++ src/components/MoviesSlider/index.js
@@ -0,0 +1,56 @@
+import Slider from 'react-slick'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import MovieItem from '../MovieItem'
+
+import './index.css'
+
+const MoviesSlider = props => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  }
+  const {moviesList} = props
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        {moviesList.map(eachItem => (
+          <MovieItem details={eachItem} key={eachItem.id} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default MoviesSlider
